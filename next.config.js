d71f1bb6b7eb3893f1a1eadb383a9bec1a394c2c
bottarocarlo <bottarocarloo@gmail.com>
2022-07-04 23:38:25 +0200

    base path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "/bottarocarlo.github.io",
+  assetPrefix: "/bottarocarlo.github.io",
 }
 
 module.exports = nextConfig
