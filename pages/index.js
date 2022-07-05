db3385a1c3478bf826c9b48d7a25dfd148b03684
Carlo Bottaro <bottarocarloo@gmail.com>
2022-07-05 17:37:43 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl  font-bold text-center py-10">Hi there, I'm Carlo! </h1>
+      <h1 className="text-3xl  font-bold text-center py-10">Hi there, I&apos;m Carlo! 👋 </h1>
     </div>
   );
 }
